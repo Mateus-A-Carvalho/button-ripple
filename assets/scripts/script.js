80e5ee5e4a632c33333205c215805a29c55dabea
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-10-29 21:00:15 -0300

    Fixing the url path

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,8 +4,6 @@ btnEl.addEventListener("mouseover", (e) => {
   let x = (e.pageX - btnEl.offsetLeft);
   let y = (e.pageY - btnEl.offsetTop);
 
-  console.log(x, y);
-
   btnEl.style.setProperty("--Xposition", x + "px");
   btnEl.style.setProperty("--Yposition", y + "px");
 })
